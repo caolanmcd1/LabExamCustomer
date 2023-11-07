4d7b1a66363db239cc7de7f9015959357415e374
CAOLAN MC DONNELL - STUDENT <g00382160@atu.ie>
2023-11-07 16:21:06 +0000

    LabExamCustomerSide

---
+++ src/main/java/com/example/labexamcustomer/LabExamCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.labexamcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labexamcustomer/LabExamCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labexamcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
